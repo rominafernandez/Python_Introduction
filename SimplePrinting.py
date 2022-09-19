637368db35eef7a32267864687610f6afdfe8b8e
Romina Fernandez <rominafernandez@outlook.de>
2022-09-19 12:35:29 +0200

    Added first exercise of Python Course

---
+++ SimplePrinting.py
@@ -0,0 +1,16 @@
+## Exercise 1 - Simple printing
+
+# Write a program that prints out at least 5 of the following:
+# Your full name
+# Your birthday
+# Your study
+# A simple sentence
+# The shout STOP!!!
+# The name of this course
+
+print("Romina-Anna-Maria Fernandez Ceballos")
+print("31.08.1994")
+print("Molecular Life Science with focus on clinical immunology")
+print("Hello, I'm a simple sentence.")
+print("STOP!!!!")
+print("Applied Bioinforatics and Biostatistics")
