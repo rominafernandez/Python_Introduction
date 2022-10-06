10014d34511459d04b00d62bde8e565ad95bb38f
Romina Fernandez <rominafernandez@outlook.de>
2022-10-06 10:42:06 +0200

    Fixed Typo in assignment

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,13 @@
 ## Getting started
 
-# Programming tutorials always start with the program called “Hello World”.
-# We will do the same.
-# You have to create a file HelloWorld.py with atom.
-# Open a new file and save it as HelloWorld.py.
-# Then add the following line to the script.
+# Programming tutorials always start with the program called “Hello World”
+# We will do the same
+# You have to create a file HelloWorld.py with atom
+# Open a new file and save it as HelloWorld.py
+# Then add the following line to the script
 print("Hello World!")
 
-# Add the following commands to the file.
+# Add the following commands to the file
 print("Jack and Jill went up a hill")
 print("to fetch a pail of water;")
 print("Jack fell down, and broke his crown,")
